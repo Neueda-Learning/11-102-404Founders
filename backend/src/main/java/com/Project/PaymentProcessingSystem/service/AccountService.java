0790fbc4dde88a2371fad4b555786222d645cc8c
sravyaMajety29 <smajety0929@gmail.com>
2026-08-03 18:58:31 +0000

    Basic mvc added

---
+++ backend/src/main/java/com/Project/PaymentProcessingSystem/service/AccountService.java
@@ -0,0 +1,51 @@
+package com.Project.PaymentProcessingSystem.service;
+
+import com.Project.PaymentProcessingSystem.model.Account;
+import com.Project.PaymentProcessingSystem.repository.AccountRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account getAccountById(Long id) {
+        return accountRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Account not found"));
+    }
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Account updateAccount(Long id, Account updatedAccount) {
+        Account existingAccount = getAccountById(id);
+
+        existingAccount.setAccountHolderName(updatedAccount.getAccountHolderName());
+        existingAccount.setCurrencyCode(updatedAccount.getCurrencyCode());
+        existingAccount.setBalance(updatedAccount.getBalance());
+
+        if (updatedAccount.getAccountStatus() != null) {
+            existingAccount.setAccountStatus(updatedAccount.getAccountStatus());
+        }
+
+        return accountRepository.save(existingAccount);
+    }
+
+    public void deleteAccount(Long id) {
+        Account existingAccount = getAccountById(id);
+        accountRepository.delete(existingAccount);
+    }
+}
